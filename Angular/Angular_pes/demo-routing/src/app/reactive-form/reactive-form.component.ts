f9a6ce2c105e4b5cf4dfc5612fdbba6a0f64ccab
pradeep007nc <pradeepnaidu2486@gmail.com>
2023-09-04 18:44:29 +0530

    main

--- Angular/Angular_pes/demo-routing/src/app/reactive-form/reactive-form.component.ts
+++ Angular/Angular_pes/demo-routing/src/app/reactive-form/reactive-form.component.ts
@@ -16,11 +16,16 @@ export class ReactiveFormComponent {
     {
       first_name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
       last_name : new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
-      prn: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]{3}\\d{1}(pg|ug)\\d{2}[a-zA-Z]{2}\\d{3}$')])
+      prn: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]{3}\\d{1}(pg|ug)\\d{2}[a-zA-Z]{2}\\d{3}$'),
+                                Validators.maxLength(13)])
     }
   );
 
   onSubmit(){
     this.route = "form";
+  }
+
+  F(){
+    return this.form_group.controls;
   }
 }
